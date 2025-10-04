9c171be8406199513f43c9d2c6b2c3e98905b498
evlx <evilnyagaming@yandex.ru>
2025-10-04 09:57:30 +0300

    Добавлена возможность фильтрации документов

--- documents/views.py
+++ documents/views.py
@@ -12,6 +12,27 @@ class DocumentListView(ListView):
     context_object_name = 'documents'
     ordering = ['-upload_date']
 
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related('employee')
+        search_query = self.request.GET.get('search_query', '')
+        doc_type = self.request.GET.get('doc_type', '')
+
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query)
+
+        if doc_type:
+            queryset = queryset.filter(document_type=doc_type)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Передаем типы документов и текущие значения фильтров в шаблон
+        context['doc_types'] = Document.DOCUMENT_TYPES
+        context['search_query'] = self.request.GET.get('search_query', '')
+        context['selected_doc_type'] = self.request.GET.get('doc_type', '')
+        return context
+
 
 class DocumentCreateView(CreateView):
     model = Document
