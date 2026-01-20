c7024fa1e14421a962ab86b3e4b7cce3e4a07b61
evlx87 <evlx1311@gmail.com>
2026-01-20 15:53:57 +0300

    Исправлена ошибка с site_update

--- organization/urls.py
+++ organization/urls.py
@@ -8,13 +8,16 @@ app_name = 'organization'
 
 urlpatterns = [
     path('', organization_info, name='organization_info'),
+    path('safety-info/edit/', safety_info_edit, name='safety_info_edit'),
+
     path('department/create/', DepartmentCreateView.as_view(), name='department_create'),
     path('department/<int:pk>/update/', DepartmentUpdateView.as_view(), name='department_update'),
     path('department/<int:pk>/delete/', DepartmentDeleteView.as_view(), name='department_delete'),
+
     path('position/create/', PositionCreateView.as_view(), name='position_create'),
     path('position/<int:pk>/update/', PositionUpdateView.as_view(), name='position_update'),
     path('position/<int:pk>/delete/', PositionDeleteView.as_view(), name='position_delete'),
-    path('safety-info/edit/', safety_info_edit, name='safety_info_edit'),
+
     path('site/create/', SiteCreateView.as_view(), name='site_create'),
     path('site/<int:pk>/update/', SiteUpdateView.as_view(), name='site_update'),
     path('site/<int:pk>/delete/', SiteDeleteView.as_view(), name='site_delete'),
