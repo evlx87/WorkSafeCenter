ab6837627ae02c0b4a12de8cc7ee11263d7ef110
evlx <evilnyagaming@yandex.ru>
2026-01-26 21:47:09 +0300

    Добавлено пустое приложение авторизации

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'accounts.apps.AccountsConfig',
     'employees.apps.EmployeesConfig',
     'documents.apps.DocumentsConfig',
     'incidents.apps.IncidentsConfig',
