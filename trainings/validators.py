2f8c9d60a7bf18174a8bcf0e7c00b4046287efc5
evlx <evilnyagaming@yandex.ru>
2026-02-18 14:56:58 +0300

    Улучшение валидации размера файлов

--- trainings/validators.py
+++ trainings/validators.py
@@ -12,7 +12,13 @@ def validate_pdf_or_image(value):
             f'Недопустимый формат файла. Разрешены: {
                 ", ".join(valid_extensions)}')
 
+    # Проверка размера файла (макс. 10 МБ)
+    if value.size > 10 * 1024 * 1024:
+        raise ValidationError('Размер файла не должен превышать 10 МБ')
+
     # Дополнительная проверка "магического числа" файла
-    if ext == '.pdf' and value.read(4) != b'%PDF':
-        raise ValidationError('Файл не является валидным PDF')
-    value.seek(0)  # Возвращаем указатель в начало
+    if ext == '.pdf':
+        header = value.read(4)
+        value.seek(0)
+        if header != b'%PDF':
+            raise ValidationError('Файл не является валидным PDF')
